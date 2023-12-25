0cf57fed4d92dbacb87ec2eb57975fa3af12b3ec
test4ment <88853792+test4ment@users.noreply.github.com>
2023-12-25 16:15:26 +0000

    working models

--- lib.py
+++ lib.py
@@ -10,7 +10,7 @@ def remove_outliers(data: pd.DataFrame) -> pd.DataFrame:
 
 @st.cache_data
 def load_wineset() -> pd.DataFrame:
-  return pd.read_csv("datasets\winetask.csv")
+  return pd.read_csv("datasets/winetask.csv")
 
 global tree_feature_importances
 tree_feature_importances = [0.06582553, 0.13270819, 0.06301554, 0.05962406, 0.07057525, 0.08254963, 0.08072115, 0.0733181, 0.0564277, 0.07822854, 0.23700631]
--- models.py
+++ models.py
@@ -1,16 +1,15 @@
-import streamlit as st
 import tensorflow as tf
 import pickle
 
 @st.cache_resource
 def load_model(model: str):
     path = "models/"
-    {
-        "NaiveBayes": lambda: pickle.load(open(path + "GaussianNB.model")),
-        "RFE_3": lambda: pickle.load(open(path + "RFE_3f.model")),
-        "RFE_6": lambda: pickle.load(open(path + "RFE_6f.model")),
-        "GradBoost": lambda: pickle.load(open(path + "GradientBoostingClassifier_v2.model")),
-        "Bagging": lambda: pickle.load(open(path + "Bagging_Tree_n23.model")),
-        "Stacking": lambda: pickle.load(open(path + "StackingClassifier_kNN_Tree_LDA.model")),
+    return {
+        "NaiveBayes": lambda: pickle.load(open(path + "GaussianNB.model", "rb")),
+        "RFE_3": lambda: pickle.load(open(path + "RFE_3f.model", "rb")),
+        "RFE_6": lambda: pickle.load(open(path + "RFE_6f.model", "rb")),
+        "GradBoost": lambda: pickle.load(open(path + "GradientBoostingClassifier_v2.model", "rb")),
+        "Bagging": lambda: pickle.load(open(path + "Bagging_Tree_n23.model", "rb")),
+        "Stacking": lambda: pickle.load(open(path + "StackingClassifier_kNN_Tree_LDA.model", "rb")),
         "DNN": lambda: tf.keras.models.load_model(path + "tf_model_5.keras"),
     }[model]()
