cb97f5f536a08885b10e194921d50b3b07b25010
test4ment <nkaliczkij@yandex.ru>
2023-12-28 15:32:33 +0600

    working

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pickle
 import streamlit as st
+import tensorflow as tf
+import keras
 
 @st.cache_resource
 def load_model(model: str):
@@ -13,9 +15,21 @@ def load_model(model: str):
         "Bagging": lambda: pickle.load(open(path + "Bagging_Tree_n23.model", "rb")),
         "Stacking": lambda: pickle.load(open(path + "StackingClassifier_kNN_Tree_LDA.model", "rb")),
         "DNN": lambda: MockTFModel(),
+        "TF_DNN": lambda: TFModelAdapter(path + "tf_model_5.h5"),
     }[model]()
 
 @st.cache_resource
 class MockTFModel:
     def predict(self, X) -> np.array:
         return pickle.load(open("models/DNN_result.np.array", "rb"))
+    
+class TFModelAdapter:
+    def __init__(self, path_to_h5: str) -> None:
+        self.model = keras.models.load_model(path_to_h5)
+    
+    def predict(self, X: np.array) -> int:
+        print(X)
+        prediction = (self.model.predict(X))
+        # print(prediction)
+        # return prediction
+        return (prediction)
